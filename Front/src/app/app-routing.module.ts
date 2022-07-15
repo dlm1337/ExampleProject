5c76a26a4c771571413da3f8e079415cc7179998
dlm1337 <61361992+dlm1337@users.noreply.github.com>
2022-07-14 22:42:05 -0400

    trying to remember angular by implementing some of the angular material designs

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 
-const routes: Routes = [{ path: 'main', component: MainContentComponent }];
+const routes: Routes = [
+  { path: 'main', component: MainContentComponent },
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
