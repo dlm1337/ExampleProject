ca99be2e1cfdc6a40a36b0de01a624d4f67eee7c
dlm1337 <61361992+dlm1337@users.noreply.github.com>
2022-10-15 11:50:12 -0400

    setting up a form to practice using everything so far.

--- Front/src/app/main-content/main-content.component.ts
+++ Front/src/app/main-content/main-content.component.ts
@@ -19,12 +19,6 @@ export class MainContentComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  tiles: Tile[] = [
-    { text: 'One', cols: 1, rows: 2, color: 'lightblue' },
-    { text: 'Two', cols: 3, rows: 1, color: 'lightgreen' },
-    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
-    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
-  ];
 
   public grabId() {
     var todo = new TodoItem();
